ecb5e3ac6dfa169f37169d4a6f83c96b1efa425b
Thien <Gaothanh7979@gmail.com>
2026-02-22 17:37:35 +0700

    Add cart feature

--- src/layouts/CustomerLayout.jsx
+++ src/layouts/CustomerLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/layout/Header'
 import Footer from '../components/layout/Footer'
+import CartBootstrap from '../components/customer/CartBootstrap'
 
 export default function CustomerLayout() {
   return (
     <div className="flex flex-col min-h-screen">
+      <CartBootstrap />
+
       {/* Header */}
       <Header />
 
