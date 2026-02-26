87ad441fe542ab3a8a09352f290d7d9c87cd98de
Thien <Gaothanh7979@gmail.com>
2026-02-26 08:57:07 +0700

    reset pasword page ui/ux enchance

--- src/pages/ResetPasswordPage.jsx
+++ src/pages/ResetPasswordPage.jsx
@@ -192,7 +192,10 @@ export default function ResetPasswordPage() {
             <button
               type="submit"
               disabled={loading}
-              className="h-10 w-full bg-black text-xs font-semibold tracking-wider text-white hover:opacity-90 disabled:opacity-50 disabled:cursor-not-allowed"
+              className=" w-full bg-yellow-400 hover:bg-yellow-500
+            text-white font-semibold
+            py-3 rounded-full transition
+            disabled:opacity-60"
             >
               {loading ? "Đang đặt lại..." : "Đặt lại mật khẩu"}
             </button>
